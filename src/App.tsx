a495a0c71f2595a0de05a0b41e97ddbf0cd57100
rg-esprit <guembrk.raslen@esprit.tn>
2024-12-29 00:51:10 +0100

    add ListGroup component and update App to display cities and countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Message from "./Message"
-
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  const items = ["Tunis", "Sfax", "Sousse", "Ariana", "Hammamet"];
+  const countries = ["Tunisia", "France", "Germany", "Italy", "Spain"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
-  return <div><Message/></div>
-
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      />
+      <ListGroup
+        items={countries}
+        heading={"Countries"}
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup(props: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  return (
+    <>
+      <h1>{props.heading}</h1>
+      <ul className="list-group">
+        {props.items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              props.onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
